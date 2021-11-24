f48d9679294c02a5870e424040593926f588fee5
Romy239 <rominamaximobadillo@gmail.com>
2021-11-23 18:43:53 -0600

    hola

---
+++ b.js
@@ -0,0 +1,37 @@
+/*let indice = 1;
+while (indice <= 100){
+    console.log(indice);
+    indice = indice + 1 ;
+}
+
+let indice = 10;
+while (indice <= 100){
+    console.log(indice);
+    indice = indice + 10;
+}
+
+const latinoamerica = ['paraguay','argentina','colombia','bolivia','brasil','ecuador'];
+let i = 0;
+while (latinoamerica.length > i){
+    console.log(latinoamerica[i]);
+    i = i + 1 ;
+}
+
+let i = 0;
+do{
+    i = i + 1;
+    console.log(i);
+        
+}while(i < 5)*/
+
+//const patron = /hola/;
+//const patron = /hola[0-9]mundo/;
+//const patron = /hola[0-9]+mundo/;
+//const patron = /^hola$/;
+//const patron = /^[0-9]+$/
+
+/*let dato;
+do{
+    dato = prompt('ingrese el valor:')
+}while(!patron.test(dato));*/
+
